757fd6742c9a741888b8814d8af51a59c8ee8c12
Blind Pole <bpole@am-j3.msk.21-school.ru>
2021-06-10 17:37:02 +0300

    add .gitignore

--- clear.sh
+++ clear.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 rm -rf data log.txt sber.db
+unzip data.zip -d data
--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install  --upgrade pip
 pip install -r requirements.txt	
-source venv/bin/activate
 
 chmod 777 main.py
 ./main.py
